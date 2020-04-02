9782b2bb1b20df64564affd2051dc4be8f6aa5c7
Jean-Marc <jm.kleger@gmail.com>
2020-04-03 00:30:20 +0200

    Build files for production

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,16 +10,5 @@
 @endsection
 
 @section('scripts')
-	{{-- // <script src="{{ asset('js/app.js') }}"></script> --}}
-	{{-- // <script src="dist/js/app.js"></script> --}}
 
-	{{-- Global configuration object --}}
-  	{{-- <script>
-    	window.config = @json($config);
-  	</script> --}}
-
-	{{-- // <script src="{{ mix('dist/js/app.js') }}"></script> --}}
-	<script>
-		// import Welcome from '../js/pages/Welcome.vue';
-	</script>
 @endsection
